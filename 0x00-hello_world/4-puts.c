f55800886b1c08f4a75bf55dc3fc5419340802b5
Mapu456 <mpaula456@hotmail.com>
2021-06-17 15:17:57 -0700

    commit1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-// write "Programming is like building a multilingual puzzle
-// use the function puts and end with the value 0
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
