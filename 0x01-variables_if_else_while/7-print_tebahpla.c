5431a43bf5d1850b8a01d5cfeea990df7d58bcb1
Mapu456 <mpaula456@hotmail.com>
2021-06-18 13:24:41 -0700

    commit3

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Write a program that prints the alphabet in lowercase.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'z'; alp >= 'a'; alp--)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
