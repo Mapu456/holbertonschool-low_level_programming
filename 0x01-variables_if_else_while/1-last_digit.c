314e2230808ac24e17d4e9d173c57f3bdfd28045
Mapu456 <mpaula456@hotmail.com>
2021-06-18 10:33:17 -0700

    commit2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-/** main- print the last digit of the number stored in the variable n
- * Return: Always 0 (Success)
- */
-
+/**
+* main - print the last digit of the number stored in the variable n
+*Return: Always 0 (Success)
+*/ 
 int main(void)
 {
 	int n, digit;
