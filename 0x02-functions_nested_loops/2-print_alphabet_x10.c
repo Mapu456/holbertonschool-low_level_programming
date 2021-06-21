a524f52b8fc51b77165e1a815580bad208015b95
Mapu456 <mpaula456@hotmail.com>
2021-06-21 13:17:53 -0700

    commit2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
 void print_alphabet_x10(void)
 {
 	char alp;
+	int i;
 
-	while (alp < 5)
+	for (i = 0; i <= 10; i++)
 	{
 		for (alp = 'a'; alp <= 'z'; alp++)
 		{
 			_putchar(alp);
 		}
 		_putchar('\n');
-		alp++;
 	}
 }
