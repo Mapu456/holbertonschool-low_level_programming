4d901bb274ad542cd5bbc3e716d666c73849cb81
Mapu456 <mpaula456@hotmail.com>
2021-06-21 08:02:21 -0700

    commit0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print if the last digit is positive, zero or negative
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char note[] = "Holberton";
+	{
+		printf(note);
+	}
+	printf("\n");
+	return (0);
+}
