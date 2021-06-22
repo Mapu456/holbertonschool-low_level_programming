ee923baed5cf2f6c5b92d76c0bff297ab4464a21
Mapu456 <mpaula456@hotmail.com>
2021-06-22 14:40:46 -0700

    commit9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,6 @@
 #include "holberton.h"
 /**
- * times_table - prints the 9 times table, starting with 0
- * @i: is int
- *Return: Always 0 (Success)
+ * times_table - prints the 9 times table
  */
 void times_table(void)
 {
