17f916052eb32f8a49b75d0618f38af0b16b72b3
Mapu456 <mpaula456@hotmail.com>
2021-06-22 09:31:24 -0700

    commit 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * _isalpha - checks for alphabetic character
- * c - the character to be checked
- *Return: 1 if c is a letter, 0 otherwise
+ * _isalpha - function to check if c is a letter, lowercase or uppercase
+ * @c: is the int that will use for the argument of the function
+ * Return: 0
  */
 int _isalpha(int c)
 {
