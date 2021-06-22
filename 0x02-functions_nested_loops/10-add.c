43b4fb061a3f8550a21eed29af698b8af4130ff8
Mapu456 <mpaula456@hotmail.com>
2021-06-22 15:32:13 -0700

    commit10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * int add - prints the last digit of a number
+ * add - prints the last digit of a number
  * @a: is int
  * @b: is int
  *Return: value of the last digit
