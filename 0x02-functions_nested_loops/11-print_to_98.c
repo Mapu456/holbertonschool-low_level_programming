0f810f2f8db6a7f8ca7ea9fb5782097b419697b6
Mapu456 <mpaula456@hotmail.com>
2021-06-23 08:43:27 -0700

    commit 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,21 +7,19 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n <= 98)
+	if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			_putchar(i + '0');
-			_putchar(',');
+			printf("%d, ", i);
 		}
-	}else if (n >= 98)
+	} else if (n >= 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			_putchar(i + '0');
-			_putchar(',');
+			printf("%d, ", i);
 		}
-	}else
+	} else
 	{
 		_putchar(98);
 	}
