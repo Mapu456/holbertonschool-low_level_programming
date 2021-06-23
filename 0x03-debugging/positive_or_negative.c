2b344dfb9cca10af0d5dc22f254e078afc6acf1b
Mapu456 <mpaula456@hotmail.com>
2021-06-23 10:51:42 -0700

    commit0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,20 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "holberton.h"
 /**
  * main - print if the last digit is positive, zero or negative
  *Return: Always 0 (Success)
  */
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n == 0)
-		printf("%d is zero\n", n);
+	if (i == 0)
+		printf("%d is zero\n", i);
 	else
-		printf("%d is zero\n", n);
-	return (0);
+		printf("%d is zero\n", i);
 }
