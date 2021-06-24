6a7505ccdf6da650b0f9f0a18f8c71eed2e70a5b
Mapu456 <mpaula456@hotmail.com>
2021-06-24 16:58:20 -0700

    commit0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - checks digit 0 to 9
+ * @c: the int
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c == 1)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
