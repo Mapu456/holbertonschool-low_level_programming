3d81d07e43b63419de0464fb0cce6d189fa3929d
Mapu456 <mpaula456@hotmail.com>
2021-06-24 08:02:30 -0700

    commit1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - causes an infinite loop
- * @i: is the variable
  * Return: 0
  */
 int main(void)
@@ -11,8 +10,9 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-	
-	/main - causes an infinite loops/
+	/**
+	 * main - causes an infinite loop
+	 */
 	while (i < 10)
 	{
 		putchar(i);
