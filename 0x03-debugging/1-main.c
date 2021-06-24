d545012b483e0ad9df177e6b95595a28678cd5e7
Mapu456 <mpaula456@hotmail.com>
2021-06-24 07:35:29 -0700

    commit1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,14 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-	
-	/* main - causes an infinite loop */
+	/**
+	 * main - causes an infinite loop
+	 */
 	while (i < 10)
 	{
 		putchar(i);
 	}
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
