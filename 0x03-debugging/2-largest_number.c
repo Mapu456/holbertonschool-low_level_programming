4b8d6c5e650c8ad6095aa3243a757e31ebd5fbe2
Mapu456 <mpaula456@hotmail.com>
2021-06-24 11:39:19 -0700

    commit1

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,17 +11,17 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	// if a  is greater than both b and c, a is the largest
+	/* if a  is greater than both b and c, a is the largest */
 	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	// if b is greater than both a and c, b is the largest
+	/* if b is greater than both a and c, b is the largest */
 	else if (b > a && b > c)
 	{
 		largest = b;
 	}
-	// if both above conditions are false, c is the largest
+	/* if both above conditions are false, c is the largest */
 	else
 	{
 		largest = c;
