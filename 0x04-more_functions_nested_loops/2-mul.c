bcdebb30f0ad39ea21e2649231a390ab0902147a
Mapu456 <mpaula456@hotmail.com>
2021-06-24 17:21:26 -0700

    commit0

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - checks uppercase character
+ * @a: the int
+ * @b: the int
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int mul(int a, int b)
+{
+	int i;
+	i = a * b;
+	return (i);
+}
