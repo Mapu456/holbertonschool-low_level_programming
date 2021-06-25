47130f42ebed1644a02f4e6e0735333b19ddd239
Mapu456 <mpaula456@hotmail.com>
2021-06-24 18:53:22 -0700

    commit5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 
 /**
- * _isupper - checks uppercase character
- * @c: the int
- * Return: 1 if c is uppercase, 0 otherwise
+ * more_numbers - function that checks for uppercase character
+ *
  */
 void more_numbers(void)
 {
