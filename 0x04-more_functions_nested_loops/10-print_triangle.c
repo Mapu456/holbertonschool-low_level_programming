64d8fae34cae30e67fe13a3dc21bbf7f8901fc13
Mapu456 <mpaula456@hotmail.com>
2021-06-24 20:22:05 -0700

    commit5

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,23 +4,30 @@
  * print_triangle - prints a triangle
  *@size: size of the triangle
  *
- * Return: Always 0.
  */
-
 void print_triangle(int size)
 {
-	int c;
-	int i;
-
-	for (c = 1; c <= size; c++)
+	if (size <= 0)
 	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+		int c;
+
 		for (i = 1; i <= size; i++)
 		{
-			if (c <= size - i)
+			for (c = i; c < size; c++)
+			{
 				_putchar(' ');
-			else
+			}
+
+			for (c = 1; c <= i; c++)
+			{
 				_putchar('#');
+			}
+
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
