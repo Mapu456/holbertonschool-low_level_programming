75a38b9a215ebeb0ab24e00b4d96b732c6deea96
Mapu456 <mpaula456@hotmail.com>
2021-06-28 08:49:25 -0700

    commit 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer and updates the value it points to to 98
+ *
+ * @n: the int that will be updated
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
