d78836513149fba7b617e78db326db82254a942e
Mapu456 <mpaula456@hotmail.com>
2021-06-28 09:03:20 -0700

    commit 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the values of two integers.
+ *
+ * @a: the int that will be swaps
+ * @b: the int that will be swaps
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
