bf2ab053aec184d4f354ed293b7c8aebdffa7e9a
Mapu456 <mpaula456@hotmail.com>
2021-06-28 14:35:49 -0700

    commit 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,19 +3,20 @@
 #include <string.h>
 
 /**
- * _puts - returns the length of a string
- * @str: string
+ * print_rev - returns the length of a string
+ * @s: string reverse
  *
  * Return: the length of the string
  */
 void print_rev(char *s)
 {
-	int i, len=0;
+	int i, len = 0;
 
 	len = strlen(s);
 
-	for(i = len - 1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
 		printf("%c", s[i]);
 	}
+	printf("\n");
 }
