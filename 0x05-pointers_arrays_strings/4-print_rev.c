b7b281a164c896ef239cfb717b786aa9633fb694
Mapu456 <mpaula456@hotmail.com>
2021-06-28 14:03:00 -0700

    commit 1

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _puts - returns the length of a string
+ * @str: string
+ *
+ * Return: the length of the string
+ */
+void print_rev(char *s)
+{
+	printf("%s\n", s);
+}
