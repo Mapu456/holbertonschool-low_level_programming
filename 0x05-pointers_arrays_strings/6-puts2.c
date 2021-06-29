a6eb81244032b397bc32437b8b4e669a62de4105
Mapu456 <mpaula456@hotmail.com>
2021-06-28 17:19:14 -0700

    commit 5

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print only one character out of two
+ * @str: input
+ * Return: number
+ */
+void puts2(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+	{
+		if (c % 2 == 0)
+		{
+			_putchar (c + '0');
+		}
+	}
+	_putchar('\n');
+}
