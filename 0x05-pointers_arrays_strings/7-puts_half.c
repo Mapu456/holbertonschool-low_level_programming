20b5445939cce0be49c8ba7855434cc21777846e
Mapu456 <mpaula456@hotmail.com>
2021-06-29 13:17:57 -0700

    commit7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,20 +7,25 @@
  */
 void puts_half(char *str)
 {
-	int i;
-	int c;
-	int n;
+	int i = 0;
+	int c = 0;
+	int n = 0;
 
-	for (i = 0; *(str + i); i++)
+	while (str[i] != '\0')
 	{
-		if (i % 2 == 0)
+		i++;
+	}
+
+	if (i % 2 != 0)
+	{
+		n = ((i - 1) / 2);
+		_putchar (str[n]);
+	}
+	else
+	{
+		c =  i / 2;
+		for (n = c; n <= i; n++)
 		{
-			c = i / 2;
-			_putchar (str[c]);
-		}
-		else
-		{
-			n = ((i - 2) / 2);
 			_putchar (str[n]);
 		}
 	}
