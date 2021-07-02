84234e22a1f7a08d3580d0db0e4bc3297cb56e98
Mapu456 <mpaula456@hotmail.com>
2021-07-02 06:11:06 -0700

    commit1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * *_strncat - concatenates two strings
+ * @src: strings
+ * @dest: strings
+ * @n: strings
+ * Return: number
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int c;
+	int x;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	/**si el if es cierto: c = n || 1024 = 1024*/
+	for (c = 0; c < n; c++)
+	{
+		x = i + c;
+		dest[x] = src[c];
+		if (src[c] == '\0')
+			c = n;
+	}
+	return (dest);
+}
