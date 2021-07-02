5c4e9f0270a62eafe28fe446b5d5d3772052e09e
Mapu456 <mpaula456@hotmail.com>
2021-07-02 16:24:39 -0700

    commit3

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: strings
+ * @n: strings
+ * Return: number
+ */
+void reverse_array(int *a, int n)
+{
+	int j;
+	int x;
+
+	for (j = 0; j < n; j++)
+	{
+		x = a[j];
+		a[j] = a[n];
+		a[n--] = x;
+	}
+}
