2cf6b5749ffec30178af4378c5463581ff34e496
Mapu456 <mpaula456@hotmail.com>
2021-07-02 18:05:00 -0700

    commit6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * *cap_string - reverses the content of an array of integers
+ * @str: strings
+ * Return: number
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	int j;
+	int a = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+			{
+				str[i] = str[i] - 32;
+			}
+			continue;
+		}
+		if (str[i] == '\t' || str[i] == ' ' || str[i] == '.')
+		{
+			++i;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+			continue;
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+			{
+				str[i] = str[i] + 32;
+			}
+		}
+	}
+	return (str);
+}
