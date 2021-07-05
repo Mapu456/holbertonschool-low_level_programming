f3feb7a4fb0bf1f2868e4bc017b475b711c82f14
Mapu456 <mpaula456@hotmail.com>
2021-07-05 14:26:04 -0700

    commit0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
  * *_memset - fills memory with a constant byte.
  * @s: strings
@@ -9,7 +9,7 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
