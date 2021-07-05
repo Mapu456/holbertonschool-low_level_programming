8ea5bd13988ba7ad288676e73b495dd0e3d0028c
Mapu456 <mpaula456@hotmail.com>
2021-07-05 16:01:36 -0700

    commit2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * _strchr - Write a function locates a character in a string
+ * *_strchr - Write a function locates a character in a string
  * @s: strings
  * @c: strings
  * Return: char
@@ -10,7 +10,7 @@ char *_strchr(char *s, char c)
 {
 	int j;
 
-	for (j = 0; s[j] != '\0'; j++)
+	for (j = 0;s[j] != '\0'; j++)
 	{
 		if (s[j] == c)
 		{
