8b9eeb18e285d8c3f8ddba0d1fef82ef774ceb57
Mapu456 <mpaula456@hotmail.com>
2021-07-06 21:01:41 -0700

    commit100

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_number - Write a function locates a character in a string
+ * @n: strings
+ */
+void print_number(int n)
+{
+	int c;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		c = -n;
+	}
+	else
+	{
+		c = n;
+	}
+	if (c / 10)
+	{
+		print_number(c / 10);
+	}
+	_putchar((c % 10) + '0');
+}
