70a618f5c2fea8f3bdb0dee59ece9b9d466320cb
Mapu456 <mpaula456@hotmail.com>
2021-07-08 12:20:10 -0700

    commit0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - prints name
+ * @argv: array whit arguements
+ * @argc: number of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
