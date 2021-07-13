6a7ddeaebd4d27465448eef8e7393c1fcc951404
Mapu456 <mpaula456@hotmail.com>
2021-07-12 18:57:37 -0700

    commi0

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - checks digit 0 to 9
+ * @c: the int
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,2 @@
+libholberton.a, holberton.h: Create the static library libholberton.a containing
+all the functions listed below
