be5f8145f1567d78b17c5ecbc5f6efda1c232004
Mapu456 <mpaula456@hotmail.com>
2021-07-14 11:10:41 -0700

    commi2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,10 +16,9 @@ char *str_concat(char *s1, char *s2)
 	int j;
 	int n;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL || s2 == NULL)
 	{
-		s1 = "";
-		s2 = "";
+		s1 = s2 = "";
 	}
 
 	for (c = 0; s1[c] != '\0'; c++)
