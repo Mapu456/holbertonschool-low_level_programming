309b2bab538653d02d64e2b512ee074aeef8de71
Mapu456 <mpaula456@hotmail.com>
2021-07-14 11:03:59 -0700

    commi2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat -  creates an array of chars
+ * @s1: size input
+ * @s2: char
+ * Return: 0
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	int i;
+	int c;
+	int j;
+	int n;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		s1 = "";
+		s2 = "";
+	}
+
+	for (c = 0; s1[c] != '\0'; c++)
+	{
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+
+	p = malloc(sizeof(char) * c + j + 1);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < c; i++)
+		{
+			p[i] = s1[i];
+		}
+		for (n = 0; n < j; n++)
+		{
+			p[i + n] = s2[n];
+		}
+		p[i + n] = '\0';
+	}
+	return (p);
+}
