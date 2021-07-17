4d5d4eb95469b28485475817e9d2bdb7bc2dcfaf
Mapu456 <mpaula456@hotmail.com>
2021-07-17 11:55:03 -0500

    commit0

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * string_nconcat - nested loop to make grid
+ * @s1: width input
+ * @s2: width input
+ * @n: width input
+ * Return: p
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	int i, j;
+	unsigned int c, a;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (c = 0; s2[c] != '\0'; c++)
+	{
+	}
+	p = malloc(sizeof(char) * i + c + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	else if (n >= c)
+	{
+		return (s2);
+	}
+	else
+	{
+		for (j = 0; j < i; j++)
+		{
+			p[j] = s1[j];
+		}
+		for (a = 0; a < n; a++)
+		{
+			p[j + a] = s2[a];
+		}
+		p[j + a] = '\0';
+	}
+	return (p);
+}
