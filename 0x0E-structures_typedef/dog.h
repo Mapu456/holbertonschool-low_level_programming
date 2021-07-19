10a22e5a3598ffa70f3b0d229ce12751e898b303
Mapu456 <mpaula456@hotmail.com>
2021-07-19 16:03:02 -0500

    commit0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 /**
-* dog - function
-*/
+ * struct dog - function
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
