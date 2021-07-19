97b5bdddd3d6863657b3bf7e36c929a123565f92
Mapu456 <mpaula456@hotmail.com>
2021-07-19 18:36:26 -0500

    commit2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - function
+ * @d: pointer of struct dog to initialize
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+	if (d->age == 0)
+	{
+		d->age = "(nil)";
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+	if (d == NULL)
+	{
+		return;
+	}
+	printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
