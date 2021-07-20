65299cd7300f3eca797f394e8b669f394af5ea11
Mapu456 <mpaula456@hotmail.com>
2021-07-19 19:28:44 -0500

    commit2.2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,7 +20,7 @@ void print_dog(struct dog *d)
 		return;
 	}
 	printf("Name: %s\n", d->name);
-	printf("Age: %.6f\n", d->age);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
 }
 
