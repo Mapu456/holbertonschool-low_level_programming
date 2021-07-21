64d118abe1cfc54692e3b1d0a20e34c35fe9ab7b
Mapu456 <mpaula456@hotmail.com>
2021-07-21 12:45:42 -0500

    commit0

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - function
+ * @array: pointer to array
+ * @size: size of array, is number
+ * @action: pointer to conduce to function
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t c;
+
+	if (array == NULL)
+	{
+		return;
+	}
+	if (action == NULL)
+	{
+		return;
+	}
+	for (c = 0; c <= size; c++)
+	{
+		action(array[c]);
+	}
+}
