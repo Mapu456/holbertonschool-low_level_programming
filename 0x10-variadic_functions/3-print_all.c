feb48faf00efec42b6398d1436b642c1e2dcfb85
Mapu456 <mpaula456@hotmail.com>
2021-07-23 18:14:42 -0500

    commit3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_all - Entry Point
+ * @format: comma space
+ */
+void print_all(const char * const format, ...)
+{
+	va_list parametros;
+	int o = 0;
+	int i = 0;
+	char *c;
+	char *j = ", ";
+
+	va_start(parametros, format);
+
+	while (format && format[i])
+	{
+		i++;
+	}
+	while (format && format[o])
+	{
+		if (o  == (i - 1))
+		{
+			j = "";
+		}
+		switch (format[o])
+		{
+		case 'c':
+			printf("%c%s", va_arg(parametros, int), j);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(parametros, int), j);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(parametros, double), j);
+			break;
+		case 's':
+			c = va_arg(parametros, char *);
+			if (c == NULL)
+				    c = "(nil)";
+			printf("%s%s", c, j);
+			break;
+		}
+		o++;
+	}
+	printf("\n");
+	va_end(parametros);
+}
