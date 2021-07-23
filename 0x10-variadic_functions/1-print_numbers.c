9b3dc363e94c24081870a2299d23f38bbabab6f3
Mapu456 <mpaula456@hotmail.com>
2021-07-23 12:26:44 -0500

    commit2

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - Entry Point
+ * @separator: comma space
+ * @n: number of elements to be printed
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list parametros;
+	int i, c, a;
+
+	va_start(parametros, n);
+	if (separator == NULL)
+	{
+		return;
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			c = va_arg(parametros, int);
+			if (i == (n - 1))
+			{
+				printf("%d", c);
+			}
+			else
+			{
+				printf("%d %s", c, separator);
+			}
+		}
+		printf("\n");
+	}
+	va_end(parametros);
+}
