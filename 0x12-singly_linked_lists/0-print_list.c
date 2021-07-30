3f255521b8353c48733a711d8cad7947e65ab9bd
Mapu456 <mpaula456@hotmail.com>
2021-07-30 16:38:33 -0500

    add  directoyr

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - function that prints the arguments
+ * @h: is the space with the values of struct
+ *
+ * Return: the number of nodes printed
+ */
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	for (s = 0; h != NULL; s++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (s);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,5 @@
+# Welcome!
+
+Singly linked lists
+
+### 0-print_list.c : Write a function that prints all the elements of a list_t list.
