b92d11afb2f8a0c6f0c81435abcf37988ba91464
Mapu456 <mpaula456@hotmail.com>
2021-08-02 17:13:28 -0500

    task6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * pop_listint - function to return i
+ * @head: the first in the head
+ * Return: the numbers of n
+ */
+int pop_listint(listint_t **head)
+{
+	int aux1;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	aux1 = (*head)->n;
+	*head = (*head)->next;
+	return (aux1);
+}
+
