ae3f625f173aeb213e1350554b9276367e240db3
Mapu456 <mpaula456@hotmail.com>
2021-08-03 11:24:15 -0500

    task7.1

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,16 +12,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (head->next == NULL)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
-	else
+	for (i = 0; i < index; i++)
 	{
-		for (i = 0; i < index; i++)
-		{
-			head = head->next;
-		}
-		return (head);
+		head = head->next;
 	}
+	return (head);
 }
