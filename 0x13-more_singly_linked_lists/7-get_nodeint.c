324e594710d6bded5443b9b47a75ace7ab8c4922
Mapu456 <mpaula456@hotmail.com>
2021-08-03 11:19:49 -0500

    task7.1

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,9 +16,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < index; i++)
+	else
 	{
-		head = head->next;
+		for (i = 0; i < index; i++)
+		{
+			head = head->next;
+		}
 	}
 	return (head);
 }
