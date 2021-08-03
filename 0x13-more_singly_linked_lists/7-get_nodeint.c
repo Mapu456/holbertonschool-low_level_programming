b53241b0ce9e84b45c3404db938e72a0158f8ede
Mapu456 <mpaula456@hotmail.com>
2021-08-02 21:34:28 -0500

    task7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * get_nodeint_at_index - function to sum
+ * @head: the first node
+ * @index: the number that determine which is the n to prints
+ * Return: head
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+	}
+	return (head);
+}
