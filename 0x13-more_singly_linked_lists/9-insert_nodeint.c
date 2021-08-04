ca331b840d581a91e9d45335f11035d813b2517d
Mapu456 <mpaula456@hotmail.com>
2021-08-03 20:46:46 -0500

    task9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - function to sum
+ * @head: the first node
+ * @idx: the number that determine which is the n to prints
+ * @n: the value of each node
+ * Return: head
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node;
+	listint_t *temp;
+	unsigned int i = 0;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	temp = *head;
+	while (temp)
+	{
+		temp = temp->next;
+		if (i == idx - 1)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return (new_node);
+		}
+		i++;
+	}
+	return (NULL);
+}
