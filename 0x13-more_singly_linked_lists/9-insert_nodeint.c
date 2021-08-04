be8d010db1c151f1ad1f3ecd685444ba0ab87c04
Mapu456 <mpaula456@hotmail.com>
2021-08-03 21:31:32 -0500

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -45,9 +45,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		i++;
 	}
+	new_node->next = temp->next;
+    temp->next = new_node;
 	if (i < (idx - 1))
 	{
 		return(NULL);
 	}
 	return (NULL);
 }
+
