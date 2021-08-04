b322840844775210206cc23b80daaaf13cd8ab03
Mapu456 <mpaula456@hotmail.com>
2021-08-03 21:04:52 -0500

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,9 +22,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new_node->n = n;
 	temp = *head;
-	if (temp == NULL)
+	if (idx == 0)
 	{
-		return (NULL);
+		new_node->next = *head;
+		*head = new_node;
+		return(*head);
 	}
 	while (temp->next)
 	{
