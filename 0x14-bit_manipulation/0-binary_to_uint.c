7c39864e70c97b8fa6cc8d430939c5e455ad7cb1
Mapu456 <mpaula456@hotmail.com>
2021-08-05 19:05:29 -0500

    task0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * binary_to_uint - check the code
+ * @b: Always 0.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	int j;
+	int result = 0;
+	int expo = 1;
+
+	if (b)
+	{
+		for (i = 0; b[i] != '\0'; i++)
+		{
+			if (b[i] != 48 && b[i] != 49)
+			{
+				return (0);
+			}
+		}
+		for (j = (i - 1); j >= 0; j--)
+		{
+			result += ((b[j] - 48) * expo);
+			expo *= 2;
+		}
+	}
+	return (result);
+}
