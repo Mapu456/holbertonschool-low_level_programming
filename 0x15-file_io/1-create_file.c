2c58e1c4381af7a375e98cdbec71c787394b52de
Mapu456 <mpaula456@hotmail.com>
2021-08-09 18:16:23 -0500

    commit0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,12 +10,11 @@
 * create_file - function
 * @filename: name of the file
 * @text_content: number of letters that we should read and print
-* Return: 0 if it fails
+* Return: 1
 */
 int create_file(const char *filename, char *text_content)
 {
 	int fd = 0;
-	int wr = 0;
 	int i;
 
 	if (filename == NULL)
@@ -37,12 +36,9 @@ int create_file(const char *filename, char *text_content)
 		for (i = 0; text_content[i] != '\0'; i++)
 		{
 		}
-		wr = write(fd, text_content, i);
-		if (wr == -1)
-		{
-			return (-1);
-		}
+		write(fd, text_content, i);
 	}
 	close(fd);
 	return (1);
 }
+
