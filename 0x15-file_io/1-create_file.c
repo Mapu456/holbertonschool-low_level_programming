6faccba414bb747ff31187e5d30abeb03077d503
Mapu456 <mpaula456@hotmail.com>
2021-08-09 18:00:50 -0500

    commit0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
-		return (0);
+		return (-1);
 	}
 	if (text_content == NULL)
 	{
@@ -45,3 +45,4 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 	return (1);
 }
+
