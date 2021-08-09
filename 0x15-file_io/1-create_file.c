d25a70415b225108912da27bb879bb9079943d87
Mapu456 <mpaula456@hotmail.com>
2021-08-09 18:21:52 -0500

    commit0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd = 0;
+	int wr = 0;
 	int i;
 
 	if (filename == NULL)
@@ -28,17 +29,19 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		close(fd);
-		return (-1);
+		text_content = "";
 	}
 	else
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 		{
 		}
-		write(fd, text_content, i);
+		wr = write(fd, text_content, i);
+		if (wr == -1)
+		{
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
 }
-
