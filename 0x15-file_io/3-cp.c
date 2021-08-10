4e0c33cfacaa3605f962583fbbff8821f59bac57
Mapu456 <mpaula456@hotmail.com>
2021-08-09 21:35:50 -0500

    commit0

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,3 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <unistd.h>
 #include "main.h"
 /**
  * main - check the code
