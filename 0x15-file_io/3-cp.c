0f99d986b3ea9f9300834296877c1a49af6a538f
Mapu456 <mpaula456@hotmail.com>
2021-08-09 21:01:07 -0500

    commit0

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,66 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - check the code
+* @argc: name of the file
+* @argv: number of letters that we should read and print
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from;
+	int file_to;
+	int j;
+	int i;
+	int rd;
+	int wr;
+	char *buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	rd = read(file_from, buf, 1024);
+	if (rd == -1)
+	{
+		return (0);
+	}
+	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	wr = write(file_to, buf, 1024);
+	if (wr == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	j = close(file_from);
+	i = close(file_to);
+	if (j == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", j);
+		exit(100);
+	}
+	if (i == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", j);
+		exit(100);
+	}
+	return (0);
+}
