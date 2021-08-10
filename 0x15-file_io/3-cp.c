97f0dcbac2ad5433ce56d34e61467118f42dc2fd
Mapu456 <mpaula456@hotmail.com>
2021-08-09 21:39:49 -0500

    commit0

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,50 +7,41 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from = 0, file_to = 0, j = 0, i = 0;
-	int rd = 0, wr = 0;
-	char *buf[1024];
+	int file_one, file_two, r, w, c;
+	char buffer[1024];
 
 	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+			exit(97);
+	file_one = open(argv[1], O_RDONLY);
+	if (file_one == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
+	file_two = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_two == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+			exit(99);
+
+	r = 1024;
+	while (r == 1024)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		r = read(file_one, buffer, 1024);
+		if (r == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+				exit(98);
+		w = write(file_two, buffer, r);
+		if (w == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+				exit(99);
 	}
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	rd = read(file_from, buf, 1024);
-	if (rd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	wr = write(file_to, buf, 1024);
-	if (wr == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	j = close(file_from);
-	i = close(file_to);
-	if (j == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", j);
-		exit(100);
-	}
-	if (i == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", i);
-		exit(100);
-	}
+	c = close(file_one);
+	if (c == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_one),
+			exit(100);
+	c = close(file_two);
+	if (c == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_two),
+			exit(100);
+
 	return (0);
 }
