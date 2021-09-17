c9d3474fff5f58328ebc94a58d1b55404bea7252
Mapu456 <mpaula456@hotmail.com>
2021-09-17 16:14:01 -0500

    double_list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,6 +12,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
@@ -19,8 +23,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	new_node->n = n;
 	new_node->next = *head;
-
-	*head = new_node;
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
 
 	return (*head);
 }
