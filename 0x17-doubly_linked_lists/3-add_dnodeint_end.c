4fa5874df15da8e3bb9a36bbfc802cc098ccdc4d
Mapu456 <mpaula456@hotmail.com>
2021-09-17 16:57:53 -0500

    double_list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -20,6 +20,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	new_node->n = n;
 	new_node->next = NULL;
+	new_node->prev = NULL;
 
 	if (*head != NULL)
 	{
@@ -29,6 +30,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			temp = temp->next;
 		}
 		temp->next = new_node;
+		new_node->prev = temp;
 		return (temp->next);
 	}
 	*head = new_node;
