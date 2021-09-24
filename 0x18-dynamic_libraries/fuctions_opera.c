c58eb62492e464203893e42c88135066fa407f1e
Mapu456 <mpaula456@hotmail.com>
2021-09-24 12:43:15 -0500

    dinamyc libraries

---
+++ 0x18-dynamic_libraries/fuctions_opera.c
@@ -0,0 +1,51 @@
+#include "operations.h"
+/**
+ * add - writes the character c to stdout
+ * @a: The character to print
+ * @b: The character to print
+ * Return: On success 1.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - writes the character c to stdout
+ * @a: The character to print
+ * @b: The character to print
+ * Return: On success 1.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - writes the character c to stdout
+ * @a: The character to print
+ * @b: The character to print
+ * Return: On success 1.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - writes the character c to stdout
+ * @a: The character to print
+ * @b: The character to print
+ * Return: On success 1.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - writes the character c to stdout
+ * @a: The character to print
+ * @b: The character to print
+ * Return: On success 1.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
