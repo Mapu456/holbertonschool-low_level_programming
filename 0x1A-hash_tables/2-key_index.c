dab6d18eaeb89badbd5cf2e21309a0d75cb508a3
Mapu456 <mpaula456@hotmail.com>
2021-10-09 13:04:50 -0500

    hash

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,8 +6,7 @@
   * key_index - gives the index of a key
   * @key: the key of the hash
   * @size: the size of the array
-  * Return: the index at which the key/value pair is stored in
-  * the array of the hash table
+  * Return: the index at which the key/value
   */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
