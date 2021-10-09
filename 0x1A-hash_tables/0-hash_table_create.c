19c37f9adc5288e22a076aa8c10b661227dc2cef
Mapu456 <mpaula456@hotmail.com>
2021-10-09 12:59:34 -0500

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
-	hash_table->array = calloc(size, sizeof(hash_node_t *));
+	hash_table->array = calloc(size, sizeof(hash_node_t));
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
