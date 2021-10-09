81354f05571bea5b14a341675653a21450df1556
Mapu456 <mpaula456@hotmail.com>
2021-10-09 13:09:50 -0500

    hash

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,16 +3,16 @@
 #include <stdio.h>
 #include "hash_tables.h"
 /**
-  * key_index - gives the index of a key
-  * @key: the key of the hash
-  * @size: the size of the array
-  * Return: index
-  */
+	* key_index - gives the index of a key
+	* @key: the key of the hash
+	* @size: the size of the array
+	* Return: index
+	*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-  unsigned long int hash_num, index;
+	unsigned long int hash_num, index;
 
-  hash_num = hash_djb2(key);
-  index = hash_num % size;
-  return (index);
+	hash_num = hash_djb2(key);
+	index = hash_num % size;
+	return (index);
 }
