1d8f60ce5c8abbb5fad11b7aaec6152d34db1511
Mapu456 <mpaula456@hotmail.com>
2021-10-10 10:48:27 -0500

    hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,46 +12,43 @@
   */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
-	hash_node_t *temp;
-	int index;
+	hash_node_t *new, *ptr;
+	int idx;
 
-	if (!key || key == NULL)
+	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
+		return (0);
+	idx = key_index((unsigned char *)key, ht->size);
+	ptr = ht->array[idx];
+	while (ptr != NULL)
 	{
+		if (strcmp(ptr->key, key) == 0)
+		{
+			free(ptr->value);
+			ptr->value = strdup(value);
+			return (1);
+		}
+		ptr = ptr->next;
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->key = strdup(key);
+	new->value = strdup(value);
+	if (new->key == NULL || new->value == NULL)
+	{
+		free(new->key);
+		free(new->value);
+		free(new);
 		return (0);
 	}
-	if (ht == NULL || ht->size == 0)
-	{
-		return (0);
-	}
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-	{
-		return (0);
-	}
-	new_node->key = (char *)key;
-	new_node->value = (char *)value;
-	new_node->next = NULL;
-	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = new_node;
-	}
+	new->next = NULL;
+	if (ht->array[idx] == NULL)
+		ht->array[idx] = new;
 	else
 	{
-		temp = ht->array[index];
-		while (temp->next)
-		{
-			if (temp->key == key)
-			{
-				new_node->next = temp;
-				free(temp->value);
-				temp->value = strdup(value);
-				return (1);
-			}
-			temp = temp->next;
-		}
-		temp->next = new_node;
+		/* check if the key matches*/
+		new->next = ht->array[idx];
+		ht->array[idx] = new;
 	}
 	return (1);
 }
