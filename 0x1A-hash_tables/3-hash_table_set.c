b0735ae816507d4d09930e1919f210f86eef3475
Mapu456 <mpaula456@hotmail.com>
2021-10-10 13:56:59 -0500

    hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,18 +14,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
 	hash_node_t *temp;
-	int index;
+	unsigned long int index;
 
-	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
-	{
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	}
 	index = key_index((unsigned char *)key, ht->size);
 	temp = ht->array[index];
-	/*recorra siempre y cuando el nodo este lleno*/
 	while (temp != NULL)
 	{
-		/*recorra los nodos verificando si el temp->key == key*/
 		if (strcmp(temp->key, key) == 0)
 		{
 			free(temp->value);
@@ -38,9 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-	{
 		return (0);
-	}
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
