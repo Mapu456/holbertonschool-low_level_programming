08e54d72249f0ec88d3a3c05843e40440928a00a
Mapu456 <mpaula456@hotmail.com>
2021-10-10 14:58:01 -0500

    hash

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+  * hash_table_get - gives the value of a node
+  * @ht: the key of the hash
+  * @key: the size of the array
+  * Return: the value associated with the element, or NULL
+  */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	while (ht->array[index] != NULL)
+	{
+		if (!strcmp(ht->array[index]->key, key))
+		{
+			return (ht->array[index]->value);
+		}
+	}
+	return (NULL);
+}
