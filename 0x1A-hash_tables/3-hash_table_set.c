b6c3bb2a7bd98d3f6a5099e24c4563d52635a2d7
Mapu456 <mpaula456@hotmail.com>
2021-10-10 11:27:43 -0500

    hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -38,7 +38,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		temp = ht->array[index];
 		while (temp->next)
 		{
-			if (temp->key == key)
+			if (strcmp(temp->key == key) == 0)
 			{
 				new_node->next = temp;
 				free(new_node->value);
