168d36869655680ccad4f1f51e79ee56012609fa
Mapu456 <mpaula456@hotmail.com>
2021-10-10 14:05:37 -0500

    hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -49,6 +49,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	new_node->next = ht->array[index];
-	ht->array[index]; = new_node;
+	ht->array[index] = new_node;
 	return (1);
 }
