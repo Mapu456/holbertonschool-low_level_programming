7ab2874412e8a1c363375ea53171f17af27c9b7d
Mapu456 <mpaula456@hotmail.com>
2021-10-10 13:48:55 -0500

    hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,6 +22,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	index = key_index((unsigned char *)key, ht->size);
 	temp = ht->array[index];
+	/*recorra siempre y cuando el nodo este lleno*/
+	while (temp != NULL)
+	{
+		/*recorra los nodos verificando si el temp->key == key*/
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			if (!temp->value)
+				return (0);
+			return (1);
+		}
+		temp = temp->next;
+	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
@@ -40,30 +54,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-	new_node->next = NULL;
-	if (temp == NULL)
-	{
-		/*si el node se va poner al final todos los nodos*/
-		temp = new_node;
-	}
-	else
-	{
-		/*recorra siempre y cuando el nodo este lleno*/
-		while (temp != NULL)
-		{
-			/*recorra los nodos verificando si el temp->key == key*/
-			if (strcmp(temp->key, key) == 0)
-			{
-				free(temp->value);
-				temp->value = strdup(value);
-				return (1);
-			}
-			temp = temp->next;
-		}
-		/*recorrio todo y el key no esta en los nodos*/
-		/*entonces, agregue el nuevo nodo en la mitad*/
-		new_node->next = temp;
-		temp = new_node;
-	}
+	new_node->next = temp;
+	temp = new_node;
 	return (1);
 }
