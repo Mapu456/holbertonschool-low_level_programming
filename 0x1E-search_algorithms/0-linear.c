0f6039ae80f427974fc10270c064c2bb1e9fe26e
Mapu456 <mpaula456@hotmail.com>
2022-01-16 18:05:01 +0000

    point0 and others

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,1 +1,20 @@
 #include "search_algos.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int linear_search(int *array, size_t size, int value){
+	size_t i = 0;
+	if (array == NULL){
+		return -1;
+	} else {
+		for (i = 0; i < size; i++){
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value){
+				return i;
+			}
+		}
+	}
+	return i;
+}
