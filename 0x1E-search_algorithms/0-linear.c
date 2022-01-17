6e8266a95793c3b9e3b0d3fa042008882c67223a
Mapu456 <mpaula456@hotmail.com>
2022-01-17 13:26:09 -0500

    point 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,7 +11,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (array == NULL || size == NULL || value == NULL)
+	if (array == NULL || !size || !value)
 	{
 		return (-1);
 	}
