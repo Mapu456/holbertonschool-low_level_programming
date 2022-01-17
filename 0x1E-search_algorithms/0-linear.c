031dbe3b998e8f191ad1ac985f8c6e13c9d7d0a3
Mapu456 <mpaula456@hotmail.com>
2022-01-17 13:22:43 -0500

    point 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,17 +7,24 @@
  * @value: search value.
  * Return: index of search value in array or -1 if doesn't exist in array.
  */
-int linear_search(int *array, size_t size, int value){
+int linear_search(int *array, size_t size, int value)
+{
 	size_t i = 0;
-	if (array == NULL){
-		return -1;
-	} else {
-		for (i = 0; i < size; i++){
+
+	if (array == NULL || size == NULL || value == NULL)
+	{
+		return (-1);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
 			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-			if (array[i] == value){
-				return i;
+			if (array[i] == value)
+			{
+				return (i);
 			}
 		}
 	}
-	return -1;
+	return (-1);
 }
