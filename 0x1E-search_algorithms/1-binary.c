8e6b990a7b0ae5d17954dd70e70b471e825c9c00
Mapu456 <mpaula456@hotmail.com>
2022-01-18 12:16:35 -0500

    point 0

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,1 +1,46 @@
 #include "search_algos.h"
+/**
+ * binary_search - function that searches for a value
+ * in an array of integers using the Linear search.
+ * @array: array of integers.
+ * @size: size of array.
+ * @value: search value.
+ * Return: index of search value in array or -1 if doesn't exist in array.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t l, i = 0;
+	size_t r = size - 1;
+	size_t m = 0;
+
+	if (array == NULL || !size || !value)
+	{
+		return (-1);
+	}
+	else
+	{
+		while (l <= r)
+		{
+			printf("Searching in array: ");
+			for (i = l; i <= r; i++)
+			{
+				if (i != r)
+				{
+					printf("%d,", array[i]);
+				}
+				else
+				{
+					printf("%d\n", array[i]);
+				}
+			}
+			m = l + (r - l) / 2;
+			if (array[m] == value)
+				return (m);
+			if (array[m] < value)
+				l = m + 1;
+			else
+				r = m - 1;
+		}
+	}
+	return (-1);
+}
